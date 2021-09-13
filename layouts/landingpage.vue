01045d10c93b0d0e6c56540dde4e5e5f9ac4b026
fernando rrrrr <fmateosc@gmail.com>
2021-09-13 20:35:36 +0100

    AÑADIDA LANDING PAGE

---
+++ layouts/landingpage.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="wrapper">
+    <nuxt />    
+  </div>
+</template>
+
+<script>
+/* eslint-disable no-new */
+import PerfectScrollbar from "perfect-scrollbar";
+import "perfect-scrollbar/css/perfect-scrollbar.css";
+
+function hasElement(className) {
+  return document.getElementsByClassName(className).length > 0;
+}
+
+function initScrollbar(className) {
+  if (hasElement(className)) {
+    new PerfectScrollbar(`.${className}`);
+  } else {
+    // try to init it later in case this component is loaded async
+    setTimeout(() => {
+      initScrollbar(className);
+    }, 100);
+  }
+}
+
+import { SlideYDownTransition, ZoomCenterTransition } from "vue2-transitions";
+
+export default {
+  name: "landingpage",
+  data() {
+    return {};
+  },
+
+  methods: {
+    toggleSidebar() {
+      if (this.$sidebar.showSidebar) {
+        this.$sidebar.displaySidebar(false);
+      }
+    }
+  },
+  mounted() {
+    
+  }
+};
+</script>
+
+<style lang="scss">
+$scaleSize: 0.95;
+
+@keyframes zoomIn95 {
+  from {
+    opacity: 0;
+    transform: scale3d($scaleSize, $scaleSize, $scaleSize);
+  }
+
+  to {
+    opacity: 1;
+  }
+}
+
+.main-panel .zoomIn {
+  animation-name: zoomIn95;
+}
+
+@keyframes zoomOut95 {
+  from {
+    opacity: 1;
+  }
+
+  to {
+    opacity: 0;
+    transform: scale3d($scaleSize, $scaleSize, $scaleSize);
+  }
+}
+
+.main-panel .zoomOut {
+  animation-name: zoomOut95;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,61 @@
 <template>
-  <div class="row"><h3></h3></div>
+  <div>
+    <NavOne />
+    <BannerOne />
+    <Features />
+    <Counter />
+    <ServiceOne />
+    <ServiceTwo />    
+    <Screenshots />
+    <Pricing />
+    <Testimonial />
+    <ClientsLogo />
+    <Footer />
+  </div>
 </template>
 
-
 <script>
-    export default {
-        middleware: "authenticated",
-        mounted() {
-            $nuxt.$router.push("/dashboard");
-        }
-    }
+import "@/assets/css/bootstrap.min.css";
+import "@/assets/css/font-awesome.min.css";
+import "@/assets/plugins/mediabox/mediabox.min.css";
+import "@/assets/css/style.css";
+import "@/assets/css/responsive.css";
+import "@/assets/css/custom.css";
+
+import "@/assets/plugins/mediabox/mediabox.min.js";
+
+import NavOne from "../components/NavOne";
+import BannerOne from "../components/BannerOne";
+import Footer from "../components/Footer";
+import Features from "../components/Features";
+import Counter from "../components/Counter";
+import ServiceTwo from "../components/ServiceTwo";
+import ServiceOne from "../components/ServiceOne";
+import Video from "../components/Video";
+import Screenshots from "../components/Screenshots";
+import Pricing from "../components/Pricing";
+import Testimonial from "../components/Testimonial";
+import ClientsLogo from "../components/ClientsLogo";
+import BlogHome from "../components/BlogHome";
+import DownloadApp from "../components/DownloadApp";
+export default {
+  middleware: "index",
+  layout: "landingpage",
+  components: {
+    DownloadApp,
+    BlogHome,
+    ClientsLogo,
+    Testimonial,
+    Pricing,
+    Screenshots,
+    Video,
+    ServiceOne,
+    ServiceTwo,
+    Counter,
+    Features,
+    Footer,
+    BannerOne,
+    NavOne
+  }
+};
 </script>
